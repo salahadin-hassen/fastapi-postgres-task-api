d5847dde2bbcd376bd1b98d066c403fe89587001
salahadin-hassen <salahadinhassenhaji@gmail.com>
2026-09-07 22:42:13 +0300

    feat: persist tasks in PostgreSQL

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-from database import engine
-class Task(BaseModel):
-    title: str
-    description: str
+from database import engine, SessionLocal
+from models import Base, Task
+class TaskCreate(BaseModel):
+    title: str              
+    description: str  
     completed: bool
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+       yield db
+    finally:
+        db.close()
+ 
 
 @app.get("/")
 def read_root():
@@ -17,12 +26,16 @@ def read_health():
 
 @app.post("/tasks")
 
-def create_tasks(task: Task):
-    task_title = task.title
-    task_description = task.description
-    task_completed = task.completed
-    return {"status": "success",
-            "message": f"Task {task_title} was created."}
+def create_tasks(task: TaskCreate, db = Depends(get_db)):
+       db_task = Task(
+        title=task.title,
+        description=task.description,
+        completed=task.completed
+    )
+       db.add(db_task)
+       db.commit()
+       return {"status": "success"}
+
 @app.get("/tasks")
 
 def read_tasks():
